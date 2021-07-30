94764e0e6764d31f7a3ca5e3502eaee04db04c9c
billy leung <bleung.uts@gmail.com>
2021-07-30 16:40:23 +0100

    Added test on IsdaRates to fetch from web

--- QuantBook/Models/Utilities.cs
+++ QuantBook/Models/Utilities.cs
@@ -18,5 +18,40 @@ namespace QuantBook.Models
 
             return null;
         }
+
+        public static int get_number_calendar_days(DateTime startDate, DateTime endDate)
+        {
+            DateTime date = startDate;
+            int num = 0;
+            while (date <= endDate)
+            {
+                date = date.AddDays(1);
+                num++;
+            }
+            return num - 1;
+        }
+
+        public static DateTime get_previous_workday(DateTime date)
+        {
+            do
+            {
+                date = date.AddDays(-1);
+            }
+            while (date.DayOfWeek == DayOfWeek.Saturday ||
+                   date.DayOfWeek == DayOfWeek.Sunday ||
+                   IsHoliday(date));
+
+            return date;
+        }
+
+        private static bool IsHoliday(DateTime date)
+        {
+            string ss = @"SELECT * FROM Holiday WHERE HDate = '" + date + "'";
+            //DataTable holidayTable = xu_data.sql_load(ss);
+            bool res = false;
+            //if (holidayTable.Rows.Count > 0)
+            //    res = true;
+            return res;
+        }
     }
 }
