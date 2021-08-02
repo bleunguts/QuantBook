3576712302779ba0d05746ea14159c821fe2f837
billy leung <bleung.uts@gmail.com>
2021-08-02 14:07:33 +0100

    WIP Addded Trading SignalHelper and BackTestStrategyHelper

---
+++ QuantBook.Tests/TradingTests.cs
@@ -0,0 +1,39 @@
+﻿using Caliburn.Micro;
+using NUnit.Framework;
+using QuantBook.Models.Strategy;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuantBook.Tests
+{
+    public class TradingTests
+    {
+        #region Backtesting (BacktestHelper)
+        [Test]
+        [Ignore("WIP")]
+        public void WhenComputingLongShortPnl()
+        {
+            var input = new BindableCollection<SignalEntity>();
+            StrategyTypeEnum strategyType =  StrategyTypeEnum.NotImplemented;
+            BacktestHelper.ComputeLongShortPnl(input, 1000.0, 100.0, 150.0, strategyType, false);
+        }
+
+        #endregion
+
+        #region trading signals (SignalHelper)
+
+        [Test]
+        [Ignore("WIP")]
+
+        public void WhenGettingSignalUsingLinearRegression()
+        {
+            var input = new BindableCollection<SignalEntity>();
+            SignalHelper.GetSignal(input, 1, SignalTypeEnum.LinearRegression);
+        }
+
+        #endregion
+    }
+}
--- README.md
+++ README.md
@@ -6,14 +6,15 @@ Skipped these sections below:
 * Machine Learning pg 285
 * Data Visualization Chapter pg143
 	* Implemented MSChart and ignored the rest
-
+* Trading Strategies and Backtesting
+	* Backtesting System Implementation
+	* Backtesting System Applications
+	* Pairs Trading
+	* Backtesting for Pairs Trading
 * Pricing European Options using QuantLib pg 417
 	* Implement other engines Binomiall Jarrow Rudd, Binomial Additive Quiprobabilities, Binomial Trigeorgis,
 		Binomial Tian, Binomial Leisen Remier, Binomial Joshi, FiniteDifference, Integral, PseudoMonto Carlo, Quasi MonteCarlo
 * Pricing Fixed Income 	
-	* CDS Pricing Engine largely done
-		* Use InterpolatedHazardRate curve by constructing a list of hazard rates by building CDS and getting the impliedHazardRate
-		* last todo CDS Pricing on UI to use spread based hazard term structure instead of fixed hazard rate
     * Callable Bonds
 	* Convertible Bonds			
 * Pricing Exotic Options using QuantLib (I already wrote the coded version, this about using the lib) pg 431
